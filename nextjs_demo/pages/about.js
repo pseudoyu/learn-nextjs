d1c8f4daf7ce394288e15724b8e277369389f86e
pseudoyu <pseudoyu@connect.hku.hk>
2022-06-18 00:52:23 +0800

    feat: migrate projects from learn-react repo

---
+++ nextjs_demo/pages/about.js
@@ -0,0 +1,13 @@
+const about = () => {
+    return (
+        <div>
+            <Head>
+                <title>About</title>
+                <meta name="keywords" content="web development, next.js" />
+            </Head>
+            <h1>About</h1>
+        </div>
+    )
+}
+
+export default about
